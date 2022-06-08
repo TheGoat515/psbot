5b2bfc80a85c1ac93c12912f87a32dedcb5306e1
TheGoat515 <107097648+TheGoat515@users.noreply.github.com>
2022-06-08 20:11:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+# main.py
 import os
+import telegram
 
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+def webhook(request):
+    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        chat_id = update.message.chat.id
+        # Reply with the same message
+        bot.sendMessage(chat_id=chat_id, text=update.message.text)
+    return "ok"
