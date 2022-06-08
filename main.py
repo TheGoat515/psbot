cfa09718a3a8134e6d36226e5375241fe82d4510
TheGoat515 <107097648+TheGoat515@users.noreply.github.com>
2022-06-08 23:14:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-# main.py
-import os
-import telegram
+mport os
+import http
 
-def webhook(request):
-    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat.id
-        # Reply with the same message
-        bot.sendMessage(chat_id=chat_id, text=update.message.text)
-    return "ok"
+from flask import Flask, request
+from werkzeug.wrappers import Response
+
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, Filters, MessageHandler, CallbackContext
+
+app = Flask(__name__)
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
+
+bot = Bot(token=os.environ["TOKEN"])
+
+dispatcher = Dispatcher(bot=bot, update_queue=None)
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+@app.post("/")
+def index() -> Response:
+    dispatcher.process_update(
+        Update.de_json(request.get_json(force=True), bot))
+
+    return "", http.HTTPStatus.NO_CONTENT
